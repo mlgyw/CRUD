f931050d815e28fe9714a5a938ba87b316c3c457
mlgyw <kirya.gritsyk@mail.ru>
2022-12-17 20:21:36 +0300

    add update,cancel

--- delivery/index.js
+++ delivery/index.js
@@ -36,9 +36,49 @@ router.get("/watchStatus", async (req, res) => {
   const client = new PrismaClient();
   await client.$connect();
   const res2 = await client.order.findMany({
-  where:{
-    status:{}
-  }
+    where: {
+      status: {},
+    },
   });
   res.send(res2);
+});
+
+const status = Joi.object().keys({
+  status: Joi.string().required(),
+  id: Joi.number().required(),
+});
+
+router.get("/updateStatus", validate(status, "query"), async (req, res) => {
+  let status = req.query.status;
+  let id = parseInt(req.query.id)
+  const client = new PrismaClient();
+  await client.$connect();
+  const res1 = await client.order.update({
+    where: { 
+      id: id 
+    },
+    data:{ 
+        status:status 
+     }
+  });
+  res.send(res1);
+});
+
+const id = Joi.object().keys({
+  id: Joi.number().required(),
+});
+
+router.get("/cancelOrder", async (req, res) => {
+  let id = parseInt(req.query.id)
+  const client = new PrismaClient();
+  await client.$connect();
+  const res1 = await client.order.update({
+    where: { 
+      id: id 
+    },
+    data:{ 
+        status:'cancelled' 
+     }
+  });
+  res.send(res1);
 });
